40f122f40a795250a77ed439c9aa87e60b1b7a59
jmn327 <173676271+JMN327@users.noreply.github.com>
2024-08-26 10:16:27 +0100

    Continue work on toggle slider for part 6. of exercise

--- javascript.js
+++ javascript.js
@@ -1,12 +1,18 @@
 const myLibrary = [];
 const addBookButton = document.querySelector("#new-book-add");
+const readCheckbox = document.querySelector(".read-checkbox");
 const addTitle = document.querySelector("#new-book-title");
 const addAuthor = document.querySelector("#new-book-author");
 const addPages = document.querySelector("#new-book-pages");
 
-let book001 = new Book("The Munchkin", "Samwise Eatspies", 312);
-let book002 = new Book("The Rotor of Chronology", "Randy Newdude", 245);
-let book003 = new Book("The Hue of Spellcasting", "Terry Rincewind", 195);
+let book001 = new Book("The Munchkin", "Samwise Eatspies", 312, true);
+let book002 = new Book("The Rotor of Chronology", "Randy Newdude", 245, false);
+let book003 = new Book(
+  "The Hue of Spellcasting",
+  "Terry Rincewind",
+  195,
+  false
+);
 
 myLibrary.push(book001);
 myLibrary.push(book002);
@@ -15,11 +21,19 @@ displayBooks();
 
 addBookButton.addEventListener("click", (event) => addBookToLibrary());
 
-function Book(title, author, pages) {
+readCheckbox.addEventListener("change", function () {
+  if (this.checked) {
+    console.log("Checkbox is checked..");
+  } else {
+    console.log("Checkbox is not checked..");
+  }
+});
+
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = false;
+  this.read = read;
   this.info = function () {
     return (
       `Title: ${this.title}
@@ -31,14 +45,13 @@ Read: ` + (this.read ? `Yes` : `no`)
 }
 
 function addBookToLibrary() {
-  if (addTitle && addAuthor && addPages ) {
-    myLibrary.push(new Book(addTitle.value,addAuthor.value,addPages.value));
+  if (addTitle && addAuthor && addPages) {
+    myLibrary.push(new Book(addTitle.value, addAuthor.value, addPages.value));
     displayBooks();
-    addTitle.value = ""
-    addAuthor.value = ""
-    addPages.value = ""
+    addTitle.value = "";
+    addAuthor.value = "";
+    addPages.value = "";
   }
-
 }
 
 function displayBooks() {
@@ -79,9 +92,30 @@ function displayBooks() {
     li.appendChild(node);
     ul.appendChild(li);
     li = document.createElement("li");
-    node = document.createTextNode(book.read ? `Yes` : `no`);
+
+    node = document.createElement("label");
+    node.classList.add("switch");
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("read-checkbox");
+    checkbox.attributes.add(`data-index ${index}`)
+    book.read ? (checkbox.checked = true) : (checkbox.checked = false);
+
+    let span = document.createElement("span");
+    span.classList.add("slider");
+    span.classList.add("round");
+
+    node.appendChild(checkbox);
+    node.appendChild(span);
+
     li.appendChild(node);
     ul.appendChild(li);
     bookCards.appendChild(ul);
   });
 }
+
+/* <label class="switch">
+  <input type="checkbox">
+  <span class="slider round"></span>
+</label> */
